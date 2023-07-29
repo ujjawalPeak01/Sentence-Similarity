09684b3fe8ef89a42a3771733ecda2553ee97207
Ujjawal Srivastava <ujjawal0911@Ujjawals-MacBook-Air.local>
2023-07-29 13:39:56 +0530

    Updated template

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+from sentence_transformers import SentenceTransformer, util
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.pipe = SentenceTransformer("##huggingface_name##")
+
+    def infer(self, inputs):
+        query = inputs["query"]
+        sentences = inputs["sentences"]
+
+        query_embedding = self.pipe.encode(query)
+        sentence_embeddings = []
+        for sentence in sentences:
+            sentence_embeddings.append(self.pipe.encode(sentence))
+
+        result = {}
+        for i in range(len(sentence_embeddings)):
+            value = util.cos_sim(query_embedding, sentence_embeddings[i]).item()
+            result[sentences[i]] = round(value, 4)
+
+        return {"result": result}
+
+    def finalize(self):
+        self.pipe = None
